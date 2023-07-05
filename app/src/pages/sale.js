3aae9e763cbe167277379aa4f9189df4b8fe056c
Asad2730 <110926054+Asad2730@users.noreply.github.com>
2023-07-05 05:55:40 -0700

    1

--- app/src/pages/sale.js
+++ app/src/pages/sale.js
@@ -47,31 +47,31 @@ const th = [
 
 export default function Sale() {
 
-  
+
   const [currentTime, setCurrentTime] = useState('');
 
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateChange = (date) => {
-      setSelectedDate(date);
+    setSelectedDate(date);
   };
 
   useEffect(() => {
-      const interval = setInterval(() => {
-          const time = new Date().toLocaleTimeString('en-US', {
-              timeZone: 'Asia/Karachi',
-              hour12: true,
-              hour: 'numeric',
-              minute: 'numeric',
-              second: 'numeric'
-          });
-          setCurrentTime(time);
-      }, 1000);
+    const interval = setInterval(() => {
+      const time = new Date().toLocaleTimeString('en-US', {
+        timeZone: 'Asia/Karachi',
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      });
+      setCurrentTime(time);
+    }, 1000);
 
-      return () => clearInterval(interval);
+    return () => clearInterval(interval);
   }, []);
 
-  
+
   return (
     <>
       <div className='flex flex-row'>
@@ -85,64 +85,69 @@ export default function Sale() {
                       <SideNav navigation={sale_navigation_items} />
                     </div>
                     <div className='flex flex-col justify-center items-center py-6 pr-6'>
-                    <div className='flex flex-col ml-4'>
-                      <div className='flex flex-row my-2 space-x-2'>
-                        <h1>Bill No.</h1>
-                        <CustomInput height={35} />
-                        <CustomInput height={35} />
-                        <h2>Date & Time</h2>
-                         
-                        <span className='block w-full rounded-md border-2 bg-white border-gray-300 py-1.5
-                                      shadow-sm placeholder:text-gray-400 px-2 ml-2' style={{height:35}}>   
-                                        <DatePicker selected={selectedDate} onChange={handleDateChange} />
-                                    </span>
-                                    <CustomInput
-                                        readOnly={false}
-                                        value={currentTime}
-                                        height={35}
-                                    />
+                      <div className='flex flex-col ml-4'>
+                        <div className='flex flex-row my-2 space-x-2'>
+                          <h1>R-Id</h1>
+                          <CustomInput height={35} />
 
+                          <h2>R-Date</h2>
+
+                          <span className='block w-full rounded-md border-2 bg-white border-gray-300 py-1.5
+                                      shadow-sm placeholder:text-gray-400 px-2 ml-2' style={{ height: 35 }}>
+                            <DatePicker selected={selectedDate} onChange={handleDateChange} />
+                          </span>
+                          <CustomInput
+                            readOnly={false}
+                            value={currentTime}
+                            height={35}
+                          />
+
+                          <h1>Ref#</h1>
+                          <CustomInput height={35} />
+
+                        </div>
+                        <div className='flex flex-row'>
+                          <span className='m-3'>Client Type</span>
+                          <CustomDropDown />
+                          <span className='m-3'>Client Name</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Sale Person</span>
+                          <CustomInput height={35} />
+                        </div>
+                        <div className='flex flex-row mt-2'>
+                          <span className='m-3'>Code</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Name</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Bonus</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Qty</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Expiry Date</span>
+                          <CustomInput height={35} />
+                          <span className='m-3'>Batch#</span>
+                          <CustomInput height={35} />
+                        </div>
+                        <div className='flex flex-row mt-2'>
+                          <CustomInput height={35} />
+                          <CustomInput type={'checkbox'} />
+                          <span className='ml-4'>Check
+                            Price</span>
+                          <span className='ml-4' />
+                          <CustomInput type={'checkbox'} />
+                          <span className='ml-4'>Search by generic</span>
+                        </div>
                       </div>
-                      <div className='flex flex-row'>
-                        <span className='m-3'>Client Type</span>
-                        <CustomDropDown />
-                        <span className='m-3'>Client Name</span>
-                        <CustomInput height={35} />
-                      </div>
-                      <div className='flex flex-row mt-2'>
-                        <span className='m-3'>Code</span>
-                        <CustomInput height={35} />
-                        <span className='m-3'>Name</span>
-                        <CustomInput height={35} />
-                        <span className='m-3'>Bonus</span>
-                        <CustomInput height={35} />
-                        <span className='m-3'>Qty</span>
-                        <CustomInput height={35} />
-                        <span className='m-3'>Disc%</span>
-                        <CustomInput height={35} />
-                        <span className='m-3'>Up</span>
-                        <CustomInput height={35} />
-                      </div>
-                      <div className='flex flex-row mt-2'>
-                        <CustomInput height={35} />
-                        <CustomInput type={'checkbox'} />
-                        <span className='ml-4'>Check
-                          Price</span>
-                        <span className='ml-4' />
-                        <CustomInput type={'checkbox'} />
-                        <span className='ml-4'>Search by generic</span>
+
+                      <div className='flex w-full items-center justify-between mb-5'>
+                        <div className='w-full'>
+                          <Table th={th} tr={tr} />
+                        </div>
                       </div>
                     </div>
 
-                    <div className='flex w-full items-center justify-between mb-5'>
-                    <div className='w-full'>
-                      <Table th={th} tr={tr} />
-                    </div>
                   </div>
-                    </div>
-                   
-                  </div>
-                 
+
                 </div>
               </div>
 
