37c671391273c356684e9ff230c0ec2151d46c3b
Asad2730 <110926054+Asad2730@users.noreply.github.com>
2023-07-19 06:09:17 -0700

    1

--- app/src/DB/loginDb.js
+++ app/src/DB/loginDb.js
@@ -1,20 +1,26 @@
 // import { ipcRenderer } from 'electron-react-ipc';
-import { ipcRenderer } from 'electron';
+// import { ipcRenderer } from 'electron';
 
 
-export const login = (name, password) => {
+// export const login = (name, password) => {
 
-  return new Promise((resolve, reject) => {
-    ipcRenderer.once('login-response', (event, response) => {
-      ipcRenderer.removeAllListeners('login-response');
-      console.log('okkkk',response)
-      if (response.error) {
-        reject(new Error(response.error));
-      } else {
-        resolve(response);
-      }
-    });
+//   return new Promise((resolve, reject) => {
+//     ipcRenderer.once('login-response', (event, response) => {
+//       ipcRenderer.removeAllListeners('login-response');
+//       console.log('okkkk',response)
+//       if (response.error) {
+//         reject(new Error(response.error));
+//       } else {
+//         resolve(response);
+//       }
+//     });
 
-    ipcRenderer.send('login-request', { name, password });
-  });
-};
+//     ipcRenderer.send('login-request', { name, password });
+//   });
+// };
+
+
+
+export const login = (name,password) =>{
+  
+}
